d0e427c1220a2e7e1082e83aa1822b2056a0eadc
litzy <litzy.balam@cbtis72.edu.mx>
2019-04-26 14:30:47 -0500

    prueba1

---
+++ app/src/main/java/com/example/litzy/masterp/ActividadListaArticulos.java
@@ -0,0 +1,13 @@
+package com.example.litzy.masterp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadListaArticulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_lista_articulos);
+    }
+}
